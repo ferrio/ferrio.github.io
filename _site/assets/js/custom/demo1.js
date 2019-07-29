a84af520b067d046871ab172352689fd66a06f9d
Wolfe <f352479@Hematite.local>
2019-07-29 13:18:51 -0800

    Form additions.

--- _site/assets/js/custom/demo1.js
+++ _site/assets/js/custom/demo1.js
@@ -49,20 +49,47 @@
     }
   });
 
+  $('#first_name').change(function() { $(this).removeClass('error'); });
+  $('#last_name').change(function() { $(this).removeClass('error'); });
+  $('#email').change(function() { $(this).removeClass('error'); });
+  $('#children_field').change(function() { $(this).removeClass('error'); });
+  $('#adult_guests').change(function() { $(this).removeClass('error'); });
+  $('#child_guests_over12').change(function() { $(this).removeClass('error'); });
+  $('#child_guests_under12').change(function() { $(this).removeClass('error'); });
+
+
   form = $('form#rsvp_form');
   url = 'https://script.google.com/macros/s/AKfycbxIo_FAMz3aVDzsZT3ZnM3dX7AibkBC2UBEfYL1g1h7igbNuE4/exec';
 
   $('#submit_rsvp').on('click', function(e) {
-    $('#form_overlay').hide();
-    $('#saving_overlay').show();
-    e.preventDefault();
-    var jqxhr = $.ajax({
-      url: url,
-      method: "GET",
-      dataType: "json",
-      data: form.serialize()
-    }).success(function() { $('#thanks_overlay').show();  $('#saving_overlay').hide(); }
-    );
+    error = false;
+
+    if (!$('#first_name').val()) { $('#first_name').addClass('error'); error=true; }
+    if (!$('#last_name').val()) { $('#last_name').addClass('error'); error=true; }
+    if (!$('#email').val()) { $('#email').addClass('error'); error=true; }
+    if (!$('#children_field').val()) { $('#children_field').addClass('error'); error=true; }
+    if (!$('#adult_guests').val()) { $('#adult_guests').addClass('error'); error=true; }
+
+    if ($('#children_field').val() == 'Yes') {
+      if (!$('#child_guests_over12').val()) { $('#child_guests_over12').addClass('error'); error=true; }
+      if (!$('#child_guests_under12').val()) { $('#child_guests_under12').addClass('error'); error=true; }
+
+    }
+
+    if (error) {
+      $('#error_msg').show();
+    } else {
+      $('#form_overlay').hide();
+      $('#saving_overlay').show();
+      e.preventDefault();
+      var jqxhr = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: form.serialize()+'&date_submitted='+Date().toLocaleString("en-US", {timeZone: "America/Anchorage"})
+      }).success(function() { $('#error_msg').hide(); $('#thanks_overlay').show();  $('#saving_overlay').hide(); }
+      );
+    }
   });
 
 })();
